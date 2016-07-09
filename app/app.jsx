91dfbb972034a0d26d1ccf56478929a85e6f2134
prtfw <mns_catdogmice@yahoo.ca>
2016-07-09 17:57:12 +0000

    first commit

---
+++ app/app.jsx
@@ -0,0 +1,34 @@
+    var React = require("react")
+    var ReactDom = require("react-dom")
+    var {Route, Router, IndexRoute, hashHistory} = require("react-router")
+
+    //load foundation 
+    //require("style!css!foundation-sites/dist/foundation.min.css")
+    
+    $(document).foundation()
+    require("style!css!sass!appcss")
+  
+
+    
+    /* //ES6 destructuring syntax 
+       var Route = require('react-router').Route ect... 
+       var obj={name: 'mads'} || var {name} = obj: <=> var name = obj.name
+    */
+    
+   
+
+   
+    ReactDom.render(
+        <h3>React BP 3</h3>,
+        document.getElementById('app')
+        
+        )
+        
+        
+        
+//when button gets clicked, call a parent function on the prop called child'name=parentfunctionName = setState
+//hook up statevar=updated name to the rendering compo = passin
+
+//containers has state, dumb compo don't
+//can't ever update props... you just get those, can update state tho if container
+//containers should not do much other than rendering children (should have a root div but that's about it)
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require("webpack")
+var path = require('path')
+
+module.exports ={
+    entry: [
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/foundation.min.js',
+        './app/app.jsx'
+       ], //entry file where does it start?
+    externals: {
+        jquery: 'jQuery'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+          '$': 'jquery',
+          'jQuery': 'jquery'
+        })
+    ],
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+                root: __dirname,
+                extensions: ['', '.js', '.jsx'],
+                      //file extensions to be able to process from inside file req calls
+                alias: {
+                    appcss: 'app/styles/app.scss',
+                }
+            },
+    module: {
+        loaders: [
+                {
+                    loader: 'babel-loader', //same name as in package.json
+                    query: {
+                        presets: ['react', 'es2015', 'stage-0'] //babel loader: parse files through react translator and run them through es2015 
+                    },
+                    test: /\.jsx?$/,  //what files to laod? //=open/end regex, escape. > look for jsx files, ?=at the end of file name
+                    exclude: /(node_modules|bower_components)/ //ignore node modules folder or bower components for the translation
+                    
+                }
+        ]
+    },
+    sassLoader: {
+        includePaths: [path.resolve(__dirname, './node_modules/foundation-sites/scss')] //combine 2 paths
+    },
+    devtool: 'cheap-module-eval-source-map'
+}
